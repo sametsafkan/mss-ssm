ea7863e716f1c9dace715b7cd4e6d47b1bfe6236
Samet Safkan <sametsafkan@gmail.com>
2020-04-25 10:34:55 +0300

    initial commit.

---
+++ src/main/java/com/sametsafkan/mssssm/MssSsmApplication.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametsafkan/mssssm/MssSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
